27ae1583abd4988eca819cb0cdae80bfaf4f57c5
Unknown <burin010n@gmail.com>
2017-12-18 17:20:18 +0700

    add temp & humid status

--- root/models.py
+++ root/models.py
@@ -4,11 +4,13 @@ from django.db import models
 
 
 class State(models.Model):
-    state = models.CharField(max_length=10, default='on')
-    using_setting = models.CharField(max_length=100, default = '')
+	state = models.CharField(max_length=10, default='on')
+	using_setting = models.CharField(max_length=100, default = '')
+	cur_humid = models.IntegerField(default = -100)
+	cur_temp = models.IntegerField(default = -100)
 
-    def __str__(self):
-    	return "state: " + self.state + ", setting: " + str(self.using_setting)
+	def __str__(self):
+		return "state: " + self.state + ", setting: " + str(self.using_setting)
 
 
 class Setting(models.Model):
